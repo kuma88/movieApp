752e1ebcb8c7c50858d9874a4221230100e5face
kuma88 <kumarasond@gmail.com>
2021-02-22 17:55:22 +0800

    installed styledcomponents and tested

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
 import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 import Hello from "./components/hello";
-
+import styled from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
 
-      <Hello name="Kuma"></Hello>
-
-      <Image source={ require("./assets/6320628_150.jpg")} />
-
-    </View>
+    <Main>
+      <SomeText>
+        <Hello name='kuma'/>
+      </SomeText>
+    </Main>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
 
-});
+// });
+
+const Main = styled.View `
+flex : 1;
+background-color :lightgray; 
+align-items :center;
+justify-content: center;
+`;
+
+const SomeText = styled.Text`
+color : blue;
+font-size :30px;
+`;
