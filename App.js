611261769d4475bd57a08aa19dd700e10a1e9885
kuma88 <kumarasond@gmail.com>
2021-02-22 14:29:35 +0800

    add a local image testing

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import Hello from "./components/hello";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Hello name="Kuma"></Hello>
+
+      <Image source={ require("./assets/6320628_150.jpg")} />
+
     </View>
   );
 }
@@ -14,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
 });
